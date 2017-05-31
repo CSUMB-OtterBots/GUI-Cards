f46159532763f8bdfe6e87701a2fda84548f9026
cayuse <cayuse@mac.com>
2017-05-30 17:17:06 -0700

    adds a couple more methods from the assignment

--- phase2/src/Phase2.java
+++ phase2/src/Phase2.java
@@ -115,78 +115,24 @@ class GUICard
          iconsLoaded = true;
       }
    }
+   static private int valueAsInt(Card card)
+   {
+      return java.util.Arrays.asList(Card.values).indexOf(card.getValue());
+   }
+   
+   static private int suitAsInt(Card card)
+   {
+      return card.getSuit().ordinal(); // returns suit index
+      
+   }
+   static public Icon getIcon(Card card)
+   {
+      return iconCards[valueAsInt(card)][suitAsInt(card)];
+   }
+   
 }
 
 
-
-
-
-/*
- * 
- * public class main
-{
-   static final int NUM_CARD_IMAGES = 57; //Every typical card and back of card
-   static Icon[] icon = new ImageIcon[NUM_CARD_IMAGES];
-   
-   static void loadCardIcons()
-   {
-      String fileExt = ".gif";
-      int index = 0;
-      
-      for (int i = 0; i < 4; i++)
-      {
-         for (int j = 0; j < 14; j++)
-         {
-            icon[index] = new ImageIcon("images/" + turnIntIntoCardValue(j) + turnIntIntoCardSuit(i) + fileExt);
-            System.out.println(turnIntIntoCardValue(j) + turnIntIntoCardSuit(i) + fileExt);
-            index++;
-         }
-      }
-      icon[index] = new ImageIcon("BK.gif"); //Back of card (Last)
-   }
-   
-   static String turnIntIntoCardValue(int k) // Helper Method
-   {
-      String[] cardValues =
-      { "A", "2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K", "X"};
-      return cardValues[k];
-   }
-   
-   static String turnIntIntoCardSuit(int j) //Helper Method
-   {
-      String[] cardSuits = {"C", "D", "H", "S"};
-      return cardSuits[j];
-   }
-
-   public static void main(String[] args)
-   {
-      loadCardIcons();
-      
-      JFrame frmMyWindow = new JFrame("Card Room");
-      frmMyWindow.setSize(1150,650);
-      frmMyWindow.setLocationRelativeTo(null);
-      frmMyWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-      
-      FlowLayout layout = new FlowLayout(FlowLayout.CENTER, 5, 20);
-      frmMyWindow.setLayout(layout);
-      
-      JLabel[] labels = new JLabel[NUM_CARD_IMAGES];
-      for (int i = 0; i < NUM_CARD_IMAGES; i++)
-      {
-         labels[i] = new JLabel(icon[i]);
-      }
-      
-      for (int i = 0; i < NUM_CARD_IMAGES; i++)
-      {
-         frmMyWindow.add(labels[i]);
-      }
-      
-      frmMyWindow.setVisible(true);
-   }
-}
-
- * 
- */
 
 
 /*
